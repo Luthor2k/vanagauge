358681805b7d4a6ec4452788ac9636fb2390aea0
Luthor2k <arthur.hazleden@gmail.com>
2024-08-20 17:46:17 -0700

    shutdown

--- shutdownService.py
+++ shutdownService.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python3
 
-#wait for 
+#wait for gpio edge to trigger a safe shutdown
 
 import time
 import RPi.GPIO as GPIO 
 
-# Pin definition
 shutdown_pin = 17
-
-# Suppress warnings
 GPIO.setwarnings(False)
-
-# Use "GPIO" pin numbering
 GPIO.setmode(GPIO.BCM)
-
-# Use built-in internal pullup resistor so the pin is not floating
-# if using a momentary push button without a resistor.
 GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Use Qwiic pHAT's pullup resistor so that the pin is not floating
-GPIO.setup(shutdown_pin, GPIO.IN)
-
-# modular function to shutdown Pi
 def shut_down():
     print("shutting down")
     command = "/usr/bin/sudo /sbin/shutdown -h now"
@@ -30,22 +18,11 @@ def shut_down():
     output = process.communicate()[0]
     print(output)
 
-
-
-
 while True:
-    #short delay, otherwise this code will take up a lot of the Pi's processing power
-    time.sleep(0.5)
-
-    # wait for a button press with switch debounce on the falling edge so that this script
-    # is not taking up too many resources in order to shutdown the Pi safely
     channel = GPIO.wait_for_edge(shutdown_pin, GPIO.RISING, bouncetime=200)
 
     if channel is None:
-        print('Timeout occurred')
+        print('channel is None')
     else:
-        print('Edge detected on channel', channel)
-
-        # For troubleshooting, uncomment this line to output button status on command line
-        #print('GPIO state is = ', GPIO.input(shutdown_pin))
+        print('GPIO.wait_for_edge(shutdown_pin, GPIO.RISING')
         shut_down()
